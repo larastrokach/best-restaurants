20752bf8e173cf26924415752f4e66395b993b33
larastrokach <larastrokach@gmail.com>
2023-08-21 16:36:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [restaurant, setRestaurant] = useState (0);
+  const {id, name, description, address, image} = data[restaurant];
+
+ 
+   
+
+
+  const previousRestaurant = () => {
+    setRestaurant ((restaurant => {
+      restaurant --;
+      if (restaurant < 0) {
+        restaurant = data.length-1;
+      }
+      return restaurant;
+    }))
+  }
+  
+
+  const nextRestaurant = () => {
+    setRestaurant ((restaurant => {
+      restaurant ++;
+      if (restaurant > data.length-1) {
+        restaurant =0;
+      }
+      return restaurant;
+    }))
+  }
+
+  
+
+     return ( <div>
+ <div className="container" >
+      <h1>5 of the Best Restaurants in Prague</h1>
     </div>
-  );
+
+      <div className="container" >
+        <img src={image} width = "400px" alt="restaurant" />
+        </div>
+      <div className="container" >
+      <h2>{id}. {name}</h2>
+    </div>
+    <div className="container" >
+      <p>{description}</p>
+    </div>
+    <div className="container" >
+      <p>{address}</p>
+    </div>
+
+    <div className="btn container">
+      <button onClick ={previousRestaurant}>Previous</button>
+      <button onClick ={nextRestaurant}>Next</button>
+    </div>
+    </div>)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data = [
+
+  {
+    id: 1,
+    name: "Kantyna",
+    description: "First up on the list: my most-recommended restaurant for tourists looking for something delicious to eat near Prague’s Old Town. Kantyna is an interesting dining hall-style concept where you order from the counters and pay at the end. This modern eatery offers Czech pork and beef from the grill (and oven!) loaded onto a tray. Of course, Kantyna also offers plenty of sides (often veggies and potatoes) as well as a number of delicious sides and the occasional dessert. ",
+    address: "Politických vězňů 1511/5, Prague, Czech Republic",
+    image: "https://undiscoveredpathhome.com/wp-content/uploads/2022/03/Best-Cheap-Eats-in-Prague-Kantyna.jpeg"
+  },
+
+  {
+    id: 2,
+    name: "Eska Restaurant & Bakery",
+    description: "Eska Restaurant remains one of my favorite bakeries in Prague – but they serve up so much more than the best koláč and bread in town. Their beautiful building, located in Prague’s Karlin neighborhood, is home to one of the best restaurants in Prague. Don’t trust me, trust the Michelin guide, which has consistently named it one of their Bib Gourmand picks (but also…trust me, the travel blogger who lives in Prague and eats here all the time). If you plan to stop by, make sure to put in a reservation. It gets a little busy. ",
+    address: "Pernerova 49, Prague-Karlín, Czech Republic",
+    image: "https://undiscoveredpathhome.com/wp-content/uploads/2023/02/best-restaurants-in-Prague-Eska-1160x1547.jpeg" },
+
+  {
+    id: 3,
+    name: "Strahov Monastery Brewery",
+    description: "If you’re looking for Czech beer and traditional Czech meals, you can’t miss Strahov Monastery’s brewery. This brewery and restaurant doesn’t make many lists, but it’s a surprisingly great place to settle down for a beer and some roast pork knee. When I visited for the first time, we ordered their tartar (an absolute classic for Czechs!), the schnitzel and the pork knee. I’ve made pork knee at home before, and I’m happy to admit that theirs is much better than mine. It’s made all the better by a selection of the best Czech mustards, breads and horseradish. This dish is perfect to split, but it’s not the only tasty thing on the menu. When we visited, the place was packed – and many people had ordered the ribs. It’s safe to say there are plenty of great dishes on their menu.",
+    address: "Strahovské nádvoří 301, Prague 1-Hradčany, Czech Republic",
+    image: "https://undiscoveredpathhome.com/wp-content/uploads/2023/02/best-restaurants-in-Prague-Strahov-Monastery-Brewery-1.jpeg" },
+
+  {
+    id: 4,
+    name: "Terasa U Prince – Stunning Views of Prague",
+    description: "Perched on the rooftop of 5-star Hotel U Prince at Staroměstské náměstí 29, Terasa U Prince boasts one of the best views of Old Town Square from a restaurant in Prague. Due to the location, this restaurant is quite expensive, but if you’re looking for a spectacular panoramic view of the city center, it can be worth it. While Terasa U Prince offers a wide variety of pastas, main dishes and desserts ranging from Czech to French to Italian origin, the food can be not quite as impressive as the view. Even so, this can be a great place to enjoy a romantic restaurant experience and watch the sunset.",
+    address: " Hotel U Prince at Staroměstské náměstí 29, Prague, Czech Republic",
+    image: "https://www.justapack.com/wp-content/uploads/2019/03/terasa-u-prince-restaurant-prague-1100x688.jpg"},
+
+  {
+    id: 5,
+    name: "Kro Kitchen – Best Gastropub/Rotisserie Chicken ",
+    description: "If you are looking for delicious rotisserie chicken in Prague this is THE place to go. Many restaurants try to imitate the joy and comfort of a home-cooked meal. Kro Kitchen doesn’t just try, it excels at it. The food in this intimate 24 seat restaurant is prepared with care, and that comes through in every bite. The nutritious menu offers several tantalizing options from pulled pork knuckle sandwiches to baked cauliflower. The star of the menu, however, is the rotisserie chicken. It truly is the best we have tasted in Prague. Orders come with roasted potatoes, or rice, and a nice fresh crispy side salad. You can mix and match from an exciting selection of sides and sauces to build a perfect lunch or dinner dish. We highly recommend the sriracha sauce for an extra little kick with your meal.",
+    address: "Vinohradská 1013/66, Prague, Czech Republic",
+    image: "https://www.justapack.com/wp-content/uploads/2019/10/kro-best-restaurants-prague-scaled-e1579513115698-1100x691.jpg"  }
+
+]
